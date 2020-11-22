9551856753637333a1ac9f2df92a5939f577d4ae
Sharath Cherian Thomas <sharathct22@gmail.com>
2020-11-22 11:28:50 +0530

    cleaned up

--- src/App.js
+++ src/App.js
@@ -6,41 +6,14 @@ import '@fortawesome/fontawesome-free/css/all.min.css';
 import "bootstrap-css-only/css/bootstrap.min.css";
 import "mdbreact/dist/css/mdb.css";
 
-//import { ThemeProvider } from 'styled-components';
-//import { useDarkMode } from './useDarkMode';
-//import { lightTheme, darkTheme } from './theme';
-//import { GlobalStyles } from './global';
-//import Toggle from './Toggle';
 import "./index.css"
 
-
-//import { MDBBtn, MDBInput, MDBModal, MDBModalBody, MDBModalHeader, MDBModalFooter, MDBIcon, MDBBadge, MDBContainer, MDBRow, MDBCol } from "mdbreact";
 class App extends Component{
-    constructor() {
-        super();
-        this.state = {
-            theme:'light'
-        };
-    }
-    
-    // toggleTheme=()=>{
-    //     if (this.state.theme==='light'){
-    //         this.setState({theme:'dark'});
-    //     }
-    //     else{
-    //         this.setState({theme:'light'});
-    //     }
-    // }
     render(){
         return(
-            //<ThemeProvider theme={this.state.theme === 'light' ? lightTheme : darkTheme}>
-            <>
-                
-                <BrowserRouter>
-                    <MainRouter />
-                </BrowserRouter>
-            </>
-            //</ThemeProvider>
+            <BrowserRouter>
+                <MainRouter />
+            </BrowserRouter>
         )
     }
 }
--- src/eggs.js
+++
@@ -1,4 +0,0 @@
-export var eegg1=1
-export var eegg2=1
-export var eegg3=1
-export var eegg4=1
